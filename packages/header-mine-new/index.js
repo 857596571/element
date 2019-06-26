8828762419ebecf9f893d13ef820dd7bee9d9cca
binbin <594942787@qq.com>
2019-06-26 10:35:13 +0800

    源码头部左侧菜单修改

--- packages/header-mine-new/index.js
+++
@@ -1,8 +0,0 @@
-import HeaderMineNew from './src/main';
-
-/* istanbul ignore next */
-HeaderMineNew.install = function(Vue) {
-  Vue.component(HeaderMineNew.name, HeaderMineNew);
-};
-
-export default HeaderMineNew;
--- packages/menue-mine-new/index.js
+++
@@ -1,8 +0,0 @@
-import ElMenueMineNew from './src/main';
-
-/* istanbul ignore next */
-ElMenueMineNew.install = function(Vue) {
-  Vue.component(ElMenueMineNew.name, ElMenueMineNew);
-};
-
-export default ElMenueMineNew;
